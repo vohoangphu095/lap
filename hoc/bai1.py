7299fb3232a3bb8efba670840de4fbbb90f9fe17
vohoangphu095 <vohoangphu095@gmail.com>
2023-05-23 11:57:39 +0700

    update

--- hoc/bai1.py
+++ hoc/bai1.py
@@ -1,4 +1,21 @@
-a = int(input('nhap a'))
-b = int(input('nhap b'))
-print(a+b)
-print(a-b)
+#tim chan le
+n=int(input('nhap n'))
+if n%2==0:
+    print('so chan')
+else:
+    print('so le')
+
+
+#tim nam nhuan
+
+nam=int(input(' nhap nam '))
+if nam%4==0:
+    if nam % 100 == 0:
+        if nam % 400 == 0:
+            print(f'{nam} la nam nhuan')
+        else:
+            print(f'{nam} ko phai la nam nhuan')
+    else:
+        print(f'{nam} la nam nhuan')
+else:
+    print(f'{nam} ko phai nam nhuan')
