4e671c2a89fd0cbc8cc4386a359ffd93b0abc063
vohoangphu095 <vohoangphu095@gmail.com>
2023-06-04 17:57:18 +0700

    update

--- hoc/doan_chu.py
+++ hoc/doan_chu.py
@@ -24,10 +24,15 @@ while not end_game:
         if guess == chosen_word[x]:
             display[x] = wordx
     print(display)
+
     if guess not in chosen_word:
         lives -= 1
-        print(f'you wrong, lives: {lives}',stages[lives])
+        print(f'you wrong, lives: {lives}', stages[lives])
+    if lives == 0:
+        print("you loose")
+        end_game = True
 
     if "_" not in display:
         print('you win')
         end_game = True
+
---
+++ hoc/ktra so nguyen to.py
@@ -0,0 +1,25 @@
+student_scores = {
+    "Harry": 81,
+    "Ron": 78,
+    "Hermione": 99,
+    "Draco": 74,
+    "Neville": 62,
+}
+# 🚨 Don't change the code above 👆
+
+# TODO-1: Create an empty dictionary called student_grades.
+student_grades = {}
+# TODO-2: Write your code below to add the grades to student_grades.👇
+
+for i in student_scores:
+    if 91 <= student_scores[i] <= 100:
+        student_grades[i] = 'Outstanding'
+    elif 81 <= student_scores[i] <= 90:
+        student_grades[i] = 'Exceeds'
+    elif 71 <= student_scores[i] <= 80:
+        student_grades[i] = 'apcept'
+    else:
+        student_grades[i] = 'fail'
+
+# 🚨 Don't change the code below 👇
+print(student_grades)
